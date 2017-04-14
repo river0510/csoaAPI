e92a0913683fc96055b71c733dff93b636c0ba2f
黄河 <huanghe@huanghedeMacBook-Pro.local>
2017-04-14 21:01:54 +0800

    教师管理over

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -4,6 +4,7 @@ use Think\Controller;
 
 
 header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Headers:X-Requested-With");
 class UserController extends Controller {
       
     public function login(){  	
@@ -28,6 +29,13 @@ class UserController extends Controller {
         $res=$User->where($where)->find();
         if($res){
             $isExist = 1;
+
+            // 检测是否停用 管理员不需要
+            // if($res['state'] == 0){
+            //     $data['status'] = 400;
+            //     $data['message'] = '该账户已停用，请联系管理员';
+            //     $this->ajaxReturn($data);
+            // }
             if($res['password']== $password){
                 $role_id = $res['role_id'];
                 $data=[
@@ -49,6 +57,13 @@ class UserController extends Controller {
         $res=$Teacher->where($where)->find();       
         if($res){
             $isExist = 1;
+
+            //检测是否停用
+            if($res['state'] == 0){
+                $data['status'] = 400;
+                $data['message'] = '该账户已停用，请联系管理员';
+                $this->ajaxReturn($data);
+            }            
             if($res['password']== $password){
                 $role_id = $res['role_id'];
                 $data=[
@@ -70,6 +85,13 @@ class UserController extends Controller {
         $res=$Student->where($where)->find();
         if($res){
             $isExist = 1;
+
+            // 检测是否停用
+            if($res['state'] == 0){
+                $data['status'] = 400;
+                $data['message'] = '该账户已停用，请联系管理员';
+                $this->ajaxReturn($data);
+            }            
             if($res['password']== $password){
                 $role_id = $res['role_id'];
                 $data=[
@@ -93,6 +115,7 @@ class UserController extends Controller {
             ];
         }
 
+        //记录log
         $time = time(); //获取当前时间时间戳
         if($data['status'] == 200){
             $log = [
@@ -247,9 +270,235 @@ class UserController extends Controller {
         $this->ajaxReturn($data);
     }
 
+    public function userModify(){
+        $User = M('user');
+        $Student = M('student');
+        $Teacher = M('teacher');
+        $Role = M('role');
+        $Log = M('log');
 
+        $role_id = I('post.role_id');
+        $data = [];
+        $update = [];
+        if($role_id == 2 || $role_id == 3){
+            $update['card_number'] = I('post.card_number');
+            $update['office'] = I('post.office');
+            $update['telephone'] = I('post.telephone');
+            $update['department'] = I('post.department');
+            $update['phone'] = I('post.phone');
+            $update['short_phone'] = I('post.short_phone');
+            $update['email'] = I('post.email');
+            $update['qq'] = I('post.qq');
+            $update['wechat'] = I('post.wechat');
 
+            $where['card_number'] = $update['card_number'];
+            $res = $Teacher->where($where)->save($update);
+            if($res){
+                $data = [
+                    'status'=>200,
+                    'message'=>'信息修改成功'
+                ];
+            }else{
+                $data = [
+                    'status'=>400,
+                    'message'=>'信息未修改 或 信息有误'
+                ];
+            }
+        }else if($role_id == 4){
+            $update['card_number'] = I('post.card_number');
+            $update['class'] = I('post.class');
+            $update['major'] = I('post.major');
+            $update['dorm'] = I('post.dorm');
+            $update['identity_card'] = I('post.identity_card');
+            $update['phone'] = I('post.phone');
+            $update['short_phone'] = I('post.short_phone');
+            $update['email'] = I('post.email');
+            $update['qq'] = I('post.qq');
+            $update['wechat'] = I('post.wechat');
 
+            $where['card_number'] = $update['card_number'];
+            $res = $Student->where($where)->save($update);
+            if($res){
+                $data = [
+                    'status'=>200,
+                    'message'=>'信息修改成功'
+                ];
+            }else{
+                $data = [
+                    'status'=>400,
+                    'message'=>'信息未修改或服务器连接失败'
+                ];
+            }
+        }
+        $this->ajaxReturn($data);
+    }
+    public function getTeacher(){
+        $Teacher = M('teacher');
 
+        $data = [];
+        $res = $Teacher->field('id,role_id,name,card_number,office,telephone,phone,state')->select();
+        if($res){
+            $data['status'] = 200;
+            $data['teacher'] = $res;
+        }else{
+            $data['status']=400;
+        }
+        $this->ajaxReturn($data);
+    }
+
+    public function getOneTeacher(){
+        $Teacher = M('teacher');
+
+        $id = I('get.id');
+        // $id = 1;
+
+        $data = [];
+        $res = $Teacher->where("id = $id")->field('password',true)->find();
+        if($res){
+            $data['status'] = 200;
+            $data['teacher'] = $res;
+        }else{
+            $data['status']=400;
+        }
+        $this->ajaxReturn($data);
+    }    
+
+    public function deleteTeacher(){
+        $Teacher = M('teacher');
+
+        $data = [];
+        $id = I('get.id');
+        $res = $Teacher->where("id = $id")->delete();
+        if($res){
+            $data['status'] = 200;
+            $data['message'] = '删除成功';
+        }else{
+            $data['status'] = 400;
+            $data['message'] = '删除失败，请稍后再试';
+        }
+        $this->ajaxReturn($data);
+    }
+
+    public function startTeacher(){
+        $Teacher = M('teacher');
+
+        $data = [];
+        $id = I('get.id');
+        $update['state'] = 1;
+        $res = $Teacher->where("id = $id")->save($update);
+        if($res){
+            $data['status'] = 200;
+            $data['message'] = '启用成功';
+        }else{
+            $data['status'] = 400;
+            $data['message'] = '启用失败，请稍后再试';
+        }
+        $this->ajaxReturn($data);
+    }
+
+    public function forbidTeacher(){
+        $Teacher = M('teacher');
+
+        $data = [];
+        $id = I('get.id');
+        $update['state'] = 0;
+        $res = $Teacher->where("id = $id")->save($update);
+        if($res){
+            $data['status'] = 200;
+            $data['message'] = '禁用成功';
+        }else{
+            $data['status'] = 400;
+            $data['message'] = '禁用失败，请稍后再试';
+        }
+        $this->ajaxReturn($data);        
+    }
+
+    public function importTeacher(){
+        $Teacher = M('teacher');
+
+        $upload = new \Think\Upload();// 实例化上传类
+        $upload->maxSize   =     3145728 ;// 设置附件上传大小
+        $upload->exts      =     array('xlsx','xls');// 设置附件上传类型
+        $upload->rootPath  =     './Uploads/'; // 设置附件上传根目录
+        $upload->savePath  =     ''; // 设置附件上传（子）目录
+        // 上传文件 
+        $info   =   $upload->upload();
+    
+        if(!$info) {// 上传错误提示错误信息
+            $this->error($upload->getError());
+        }
+        
+        $filetmpname = './Uploads/'.$info['uploadfile']['savepath'].$info['uploadfile']['savename'];
+        $exts = $info['uploadfile']['ext'];
+
+        import("Org.Util.PHPExcel");
+        import("Org.Util.PHPExcel.Reader.Excel2007");
+        import("Org.Util.PHPExcle.IOFactory");
+        if($exts == 'xlsx'){
+            import("Org.Util.PHPExcel.Reader.Excel5");
+            $objReader = \PHPExcel_IOFactory::createReader("excel2007");
+        }else if($exts == 'xls'){
+            import("Org.Util.PHPExcel.Reader.Excel2007");
+            $objReader = \PHPExcel_IOFactory::createReader("Excel5");
+        }
+    
+        //  $objReader=new \PHPExcel_Reader_Excel2007();
+        $objReader->setReadDataOnly(true);
+        $objPHPExcel = $objReader->load($filetmpname,$encode='utf-8');
+       
+        $sheet = $objPHPExcel->getSheet(0);
+        $highestRow = $sheet->getHighestRow(); // 取得总行数
+        
+        $highestColumn = $sheet->getHighestColumn(); // 取得总列数
+        $arrExcel = $objPHPExcel->getSheet(0)->toArray();
+
+        $isSuccess = 1; //判断是否所有数据导入成功
+
+        //excel中的数据全部存入二维数组中
+        for($i=2;$i<=$highestRow;$i++)
+        {   
+            $j=$i-2;
+            $data[$j]['card_number']= (string)$objPHPExcel->getActiveSheet()->getCell("A".$i)->getValue();
+            $data[$j]['name'] = (string)$objPHPExcel->getActiveSheet()->getCell("B".$i)->getValue();
+            $data[$j]['department'] = (string)$objPHPExcel->getActiveSheet()->getCell("C".$i)->getValue();
+            $data[$j]['office'] = (string)$objPHPExcel->getActiveSheet()->getCell("D".$i)->getValue();
+            $data[$j]['telephone'] = (string)$objPHPExcel->getActiveSheet()->getCell("E".$i)->getValue();
+            $data[$j]['phone'] = (string)$objPHPExcel->getActiveSheet()->getCell("F".$i)->getValue();
+            $data[$j]['short_phone'] = (string)$objPHPExcel->getActiveSheet()->getCell("G".$i)->getValue();
+            $data[$j]['email'] = (string)$objPHPExcel->getActiveSheet()->getCell("H".$i)->getValue();
+            $data[$j]['qq'] = (string)$objPHPExcel->getActiveSheet()->getCell("I".$i)->getValue();
+            $data[$j]['wechat'] = (string)$objPHPExcel->getActiveSheet()->getCell("J".$i)->getValue();
+            $data[$j]['comment'] = (string)$objPHPExcel->getActiveSheet()->getCell("K".$i)->getValue();
+            $data[$j]['role_id'] = (int)$objPHPExcel->getActiveSheet()->getCell("L".$i)->getValue();
+        }
+
+        foreach($data as $value){
+            $value['password'] = $value['card_number'];
+            dump($value);
+            $where['card_number'] = $value['card_number'];
+            $resFind = $Teacher->where($where)->find();
+            if($resFind){
+                $res = $Teacher->where($where)->save($value);
+                dump($resFind);
+            }else{
+                $res = $Teacher->add($value);
+            }
+            if(!$res){
+                $isSuccess = 0;
+            }            
+        }
+        if($isSuccess){
+            $returnData = [
+                'status' => 200,
+                'message' => '导入成功'
+            ];
+        }else{
+            $returnData = [
+                'status' => 400,
+                'message' => '部分数据导入失败'
+            ];
+        }
+        $this->ajaxReturn($returnData);
+    }
 
 }
